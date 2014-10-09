e36ce345fc8dbfd44d153eb1f4ed3179004d7b80
Interface3Wad14 <Interface3Wad14@gmail.com>
2014-10-09 16:40:27 +0200

    Version avec design + Login

---
+++ MVCMedicoSoft/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMedicoSoft.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        [HttpGet]
+        public ActionResult Forms()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Forms(string txtLogin, string txtPassword)
+        {
+            Utilisateur U = Utilisateur.AuthentifieMoi(txtLogin, txtPassword);
+            if (U == null)
+            {
+                ViewBag.Error = "Try Again!!!";
+                return View();
+            }
+            else
+            {
+                //1 - stocker en session le login de l'utilisateur
+                Session["login"] = U.Login;
+                //2 - rediriger vers Home/Index
+                //return View();
+                return RedirectToRoute(
+                    new { controller = "Home", action = "Index" });
+            }
+        }
+	
+        [HttpGet]
+        public RedirectToRouteResult LogOut()
+        {
+            Session["login"] = null;
+            Session.Abandon();
+
+            return RedirectToRoute
+                (
+                    new { 
+                        controller = "Home", 
+                        action = "Index" 
+                        }
+                );
+ 
+        }
+    }
+}
