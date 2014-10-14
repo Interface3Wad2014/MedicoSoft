86ce432ed4b0b0bbf0e8557f5368a65544ecd7c6
Interface3Wad14 <Interface3Wad14@gmail.com>
2014-10-14 16:29:44 +0200

    Ajout GestUser

---
+++ MVCMedicoSoft/Controllers/GestUserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMedicoSoft.Controllers
+{
+    public class GestUserController : Controller
+    {
+        //
+        // GET: /GestUser/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
