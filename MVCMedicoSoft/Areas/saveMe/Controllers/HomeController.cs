2de560881e3e7481b7a6f4a7cab7577beb642b10
Interface3Wad14 <Interface3Wad14@gmail.com>
2014-10-21 12:17:55 +0200

    Ajout Localisation SaveMe

---
+++ MVCMedicoSoft/Areas/saveMe/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMedicoSoft.Areas.saveMe.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /saveMe/Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string txtregnat)
+        {
+            ViewBag.step = 2;
+            Personne p = Personne.getInfo(txtregnat);
+            return View("rescue", p);
+        }
+	}
+}
---
+++ MVCMedicoSoft/Areas/saveMe/saveMeAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MVCMedicoSoft.Areas.saveMe
+{
+    public class saveMeAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "saveMe";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "saveMe_default",
+                "saveMe/{controller}/{action}/{id}",
+                new { controller="Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
